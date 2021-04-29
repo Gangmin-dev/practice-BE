776f9382bed8cf9c6f7cc03bc0b97e0722ad7d93
Gangmin-dev <vinu10045@snu.ac.kr>
2021-04-29 12:23:43 +0900

    Add parts APIs that include each of one Query and two Query network

--- routes/parts.js
+++ routes/parts.js
@@ -44,4 +44,87 @@ router.get("/", function (req, res, next) {
   );
 });
 
+router.get("/twoQuery", function (req, res, next) {
+  if (!req.query) {
+    res.status(400).send("there is no queryString");
+  }
+  if (!req.query.course_id) {
+    res.status(400).send("there is no course_id query data");
+  }
+  db.query(
+    `SELECT id, number, title FROM chapter WHERE course_id = ?`,
+    [req.query.course_id],
+    (err, chapters) => {
+      if (err) {
+        console.log(err);
+        next(err);
+      }
+      db.query(
+        `SELECT id, title, chapter_id FROM part WHERE chapter_id in (SELECT id FROM chapter WHERE course_id = ?)`,
+        [req.query.course_id],
+        (err, parts) => {
+          if (err) {
+            console.log(err);
+            next(err);
+          }
+          console.log(parts);
+          chapters.forEach((chapter) => {
+            chapter.parts = [];
+            parts.forEach((part) => {
+              if (part.chapter_id === chapter.id)
+                chapter.parts.push({ id: part.id, title: part.title });
+            });
+          });
+          res.status(200).json(chapters);
+        }
+      );
+    }
+  );
+});
+
+router.get("/oneQuery", function (req, res, next) {
+  if (!req.query) {
+    res.status(400).send("there is no queryString");
+  }
+  if (!req.query.course_id) {
+    res.status(400).send("there is no course_id query data");
+  }
+  db.query(
+    `SELECT chapter.id, chapter.number, chapter.title, part.id as partId, part.title as partTitle FROM chapter
+    LEFT JOIN part ON part.chapter_id = chapter.id
+    WHERE chapter.course_id = ?
+    ORDER BY chapter.number`,
+    [req.query.course_id],
+    (err, chaptersWithParts) => {
+      if (err) {
+        console.log(err);
+        next(err);
+      }
+      let chapters = [];
+      let lastIndex = -1;
+      let currentChapterIndex;
+      for (let i = 0; i < chaptersWithParts.length; i++) {
+        if (
+          i === 0 ||
+          chaptersWithParts[lastIndex].id != chaptersWithParts[i].id
+        ) {
+          currentChapterIndex =
+            chapters.push({
+              id: chaptersWithParts[i].id,
+              number: chaptersWithParts[i].number,
+              title: chaptersWithParts[i].title,
+              parts: [],
+            }) - 1;
+        }
+        chapters[currentChapterIndex].parts.push({
+          id: chaptersWithParts[i].partId,
+          title: chaptersWithParts[i].partTitle,
+        });
+        lastIndex = i;
+      }
+      res.status(200).json(chapters);
+    }
+  );
+});
+
 module.exports = router;
