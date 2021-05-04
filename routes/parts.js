05f7c6d3f0bb045f64436ef885197c710bec6876
Gangmin-dev <vinu10045@snu.ac.kr>
2021-05-04 17:05:03 +0900

    Add query_with_tracing.js & Fix promise API of parts

--- routes/parts.js
+++ routes/parts.js
@@ -1,62 +1,107 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../lib/db");
-const initTracer = require("../../lib/tracing").initTracer;
+const query = require("../lib/query_with_tracing");
+const initTracer = require(".././lib/tracing").initTracer;
 const { Tags, FORMAT_HTTP_HEADERS } = require("opentracing");
 
 const tracer = initTracer("parts-api");
 
-const fetchParts = (chapter) =>
-  new Promise((resolve, reject) => {
-    db.query(
-      `SELECT id, title FROM part WHERE chapter_id = ?`,
-      [chapter.id],
-      (err, parts) => {
-        if (err) {
-          console.log(err);
-          return reject(err);
-        }
-        chapter.parts = parts;
+const fetchParts = (chapter, rootSpan) => {
+  const span = tracer.startSpan("fetch-parts", { childOf: rootSpan.context() });
 
-        resolve(parts);
-      }
-    );
+  return new Promise((resolve, reject) => {
+    query(`SELECT id, title FROM part WHERE chapter_id = ?`, [chapter.id], span)
+      .then((parts) => {
+        span.log({
+          event: "get-parts-success",
+          value: parts,
+        });
+        chapter.parts = parts;
+        span.finish();
+        return resolve(parts);
+      })
+      .catch((e) => {
+        logDatabaseError(e, span);
+        return reject(e);
+      });
+    // db.query(
+    //   `SELECT id, title FROM part WHERE chapter_id = ?`,
+    //   [chapter.id],
+    //   (err, parts) => {
+    //     if (err) {
+    //       console.log(err);
+    //       return reject(err);
+    //     }
+    //     chapter.parts = parts;
+
+    //     resolve(parts);
+    //   }
+    // );
   });
+};
 
 router.get("/", function (req, res, next) {
   const span = tracer.startSpan("use-promise");
-  if (!req.query.course_id) {
-    span.setTag(Tags.ERROR, true);
-    span.setTag(Tags.HTTP_STATUS_CODE, 400);
-    span.finish();
-    res.status(400).send("there is no course_id query data");
+
+  if (checkQueryString(req, span)) {
+    return res.status(400).send("there is no course_id query data");
   }
-  db.query(
+
+  query(
     `SELECT id, number, title FROM chapter WHERE course_id = ?`,
     [req.query.course_id],
-    (err, chapters) => {
-      if (err) {
-        console.log(err);
-        next(err);
-      }
-      Promise.all(chapters.map((chapter) => fetchParts(chapter)))
+    span
+  )
+    .then((chapters) => {
+      span.log({
+        event: "get-chapters",
+        value: chapters,
+      });
+
+      Promise.all(chapters.map((chapter) => fetchParts(chapter, span)))
         .then((v) => {
-          console.log(chapters);
+          span.log({
+            event: "fetch-parts-success",
+            value: chapters,
+          });
+          span.finish();
           res.status(200).json(chapters);
         })
-        .catch(next);
-    }
-  );
+        .catch((e) => {
+          logDatabaseError(e, span);
+          res.status(500).send(e);
+        });
+    })
+    .catch((e) => {
+      logDatabaseError(e, span);
+      res.status(500).send(e);
+    });
+  // db.query(
+  //   `SELECT id, number, title FROM chapter WHERE course_id = ?`,
+  //   [req.query.course_id],
+  //   (err, chapters) => {
+  //     if (err) {
+  //       console.log(err);
+  //       next(err);
+  //     }
+  //     Promise.all(chapters.map((chapter) => fetchParts(chapter)))
+  //       .then((v) => {
+  //         console.log(chapters);
+  //         res.status(200).json(chapters);
+  //       })
+  //       .catch(next);
+  //   }
+  // );
 });
 
 router.get("/twoQuery", function (req, res, next) {
   const span = tracer.startSpan("use-two-query");
-  if (!req.query.course_id) {
-    span.setTag(Tags.ERROR, true);
-    span.setTag(Tags.HTTP_STATUS_CODE, 400);
-    span.finish();
+
+  if (checkQueryString(req, span)) {
     res.status(400).send("there is no course_id query data");
   }
+
   db.query(
     `SELECT id, number, title FROM chapter WHERE course_id = ?`,
     [req.query.course_id],
@@ -90,12 +135,11 @@ router.get("/twoQuery", function (req, res, next) {
 
 router.get("/oneQuery", function (req, res, next) {
   const span = tracer.startSpan("use-one-query");
-  if (!req.query.course_id) {
-    span.setTag(Tags.ERROR, true);
-    span.setTag(Tags.HTTP_STATUS_CODE, 400);
-    span.finish();
+
+  if (checkQueryString(req, span)) {
     res.status(400).send("there is no course_id query data");
   }
+
   db.query(
     `SELECT chapter.id, chapter.number, chapter.title, part.id as partId, part.title as partTitle FROM chapter
     LEFT JOIN part ON part.chapter_id = chapter.id
@@ -133,5 +177,30 @@ router.get("/oneQuery", function (req, res, next) {
     }
   );
 });
+
+function logDatabaseError(e, span) {
+  span.setTag(Tags.ERROR, true);
+  span.setTag(Tags.HTTP_STATUS_CODE, 500);
+  span.log({
+    event: "DB-query-error",
+    "error.object": e,
+  });
+  span.finish();
+}
+
+function checkQueryString(req, span) {
+  if (!req.query.course_id) {
+    span.setTag(Tags.ERROR, true);
+    span.setTag(Tags.HTTP_STATUS_CODE, 400);
+    span.log({
+      event: "ERROR: no-matching-queryString",
+      queryString: req.query,
+    });
+    span.finish();
+
+    return 1;
+  }
+  return 0;
+}
 
 module.exports = router;
